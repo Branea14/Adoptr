202e2ca847d12a8e655d86e94434b2406756c296
Erika <brandonerika96@gmail.com>
2025-03-11 21:22:03 -0400

    navigation bar done

--- react-vite/src/components/Navigation/Navigation.jsx
+++ react-vite/src/components/Navigation/Navigation.jsx
@@ -1,13 +1,17 @@
-import ProfileButton from "./ProfileButton";
 import "./Navigation.css";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect, useState } from "react";
 import { approvedMatches } from "../../redux/matches";
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
+import { FaUserCircle } from 'react-icons/fa';
+import { thunkAuthenticate, thunkLogout } from "../../redux/session";
+
 
 function Navigation() {
   const dispatch = useDispatch()
+  const navigate = useNavigate()
   const [loading, setLoading] = useState(true)
+  const [shifted, setShifted] = useState(false)
 
   const currentUser = useSelector((state) => state.session.user)
   const approvedMatch = useSelector((state) => state.matches?.approvedMatches)
@@ -17,29 +21,47 @@ function Navigation() {
   useEffect(() => {
     setLoading(true)
     Promise.all([
+      dispatch(thunkAuthenticate()),
       dispatch(approvedMatches())
     ]).finally(() => setLoading(false))
   }, [dispatch])
 
+  const handleProfileIconClick = () => {
+    setShifted((prev) => !prev)
+  }
+
+  const logout = (e) => {
+    e.preventDefault();
+    dispatch(thunkLogout());
+    // closeMenu();
+    navigate('/')
+  };
+
   return (
-    <div className='container-of-headers'>
+    <div className={"container-of-headers"}>
       <div className='navbar2'>
         <img src="/logo.png" alt="Adoptr Logo" className='nav-logo'/>
         <h1 className='platform-name'>Adoptr</h1>
       </div>
-      <div className='navbar'>
+      <div className={`navbar ${shifted ? "shifted" : ""}`}>
         <ul>
           {/* <li>
             <NavLink to="/">Home</NavLink>
-          </li> */}
-          <li>
-            <ProfileButton />
-            {currentUser?.firstName}
+            </li> */}
+          <li className='profile-button-container' onClick={handleProfileIconClick}>
+            <div className={`profile-button ${shifted ? "move-profile" : ""}`}>
+              <button className="toggle-button">
+                {shifted ? "🔙" : ""}
+              </button>
+              {/* <ProfileButton /> */}
+              <FaUserCircle />
+              {currentUser?.firstName}
+            </div>
           </li>
         </ul>
         <h2>Approved Matches</h2>
             <div className="approved-match">
-              {filteredApprovedMatches.length > 0 ? (
+              {loading ? null : filteredApprovedMatches.length > 0 ? (
                 filteredApprovedMatches.map((match) => (
                   <Link to={`/pets/${match.petId}`} key={match.id} className='pet-details-link'>
                     <div key={match.id}>
@@ -48,9 +70,28 @@ function Navigation() {
                     </div>
                   </Link>
                 ))
-              ) : null}
+              ) : (<p>No approved matches yet</p>)}
             </div>
+          {shifted && (
+            <div className="additional-menu">
+              <ul>
+                <li>
+                  <Link>Manage Pet Listings</Link>
+                </li>
 
+                <li>
+                  <Link>Matches</Link>
+                </li>
+
+                <li>
+                  <Link>Edit Profile</Link>
+                </li>
+
+                <li onClick={logout}>Logout</li>
+
+              </ul>
+            </div>
+          )}
 
       </div>
 
